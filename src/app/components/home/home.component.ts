a8fd0de3786cc31d9473673efa9863fea3abcad0
ridoy.hossain <ridoy.hossain@bracits.com>
2024-03-12 09:25:28 +0600

    input how work, send data to p to c

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,76 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, CommonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  products = [1, 2, 3, 4, 5];
+  products = [
+    {
+      id: '1',
+      brand: 'US.POLO',
+      image:
+        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
+      currentPrice: '500',
+      standardPrice: '150',
+      discount: '50',
+      name: 'Men sky blue Jeans',
+    },
+    {
+      id: '2',
+      brand: 'US.POLO',
+      image:
+        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
+      currentPrice: '500',
+      standardPrice: '150',
+      discount: '50',
+      name: 'Men sky blue Jeans',
+    },
+    {
+      id: '3',
+      brand: 'US.POLO',
+      image:
+        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
+      currentPrice: '500',
+      standardPrice: '150',
+      discount: '50',
+      name: 'Men sky blue Jeans',
+    },
+    {
+      id: '4',
+      brand: 'US.POLO',
+      image:
+        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
+      currentPrice: '500',
+      standardPrice: '150',
+      discount: '50',
+      name: 'Men sky blue Jeans',
+    },
+    {
+      id: '5',
+      brand: 'US.POLO',
+      image:
+        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
+      currentPrice: '500',
+      standardPrice: '150',
+      discount: '50',
+      name: 'Men sky blue Jeans',
+    },
+    {
+      id: '6',
+      brand: 'US.POLO',
+      image:
+        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
+      currentPrice: '500',
+      standardPrice: '150',
+      discount: '50',
+      name: 'Men sky blue Jeans',
+    },
+  ];
 }
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [ProductCardComponent, MatCardModule],
+  imports: [ProductCardComponent, MatCardModule, CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  @Input() product: any;
+}
