2da792600084f51daf84f2862265008d9e51c5c1
ridoy.hossain <ridoy.hossain@bracits.com>
2024-03-12 11:50:43 +0600

    product service in inject services

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { SearchComponent } from '../search/search.component';
+import { ProductService } from '../../product.service';
 
 @Component({
   selector: 'app-home',
@@ -12,71 +13,11 @@ import { SearchComponent } from '../search/search.component';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  products = [
-    {
-      id: '1',
-      brand: 'US.POLO',
-      image:
-        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
-      currentPrice: '500',
-      standardPrice: '150',
-      discount: '50',
-      name: 'Hello',
-    },
-    {
-      id: '2',
-      brand: 'US',
-      image:
-        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
-      currentPrice: '500',
-      standardPrice: '150',
-      discount: '50',
-      name: 'Mensky',
-    },
-    {
-      id: '3',
-      brand: 'UK',
-      image:
-        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
-      currentPrice: '500',
-      standardPrice: '150',
-      discount: '50',
-      name: 'Hi',
-    },
-    {
-      id: '4',
-      brand: 'US.POLO',
-      image:
-        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
-      currentPrice: '500',
-      standardPrice: '150',
-      discount: '50',
-      name: 'blue ',
-    },
-    {
-      id: '5',
-      brand: 'US.POLO',
-      image:
-        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
-      currentPrice: '500',
-      standardPrice: '150',
-      discount: '50',
-      name: 'Men ',
-    },
-    {
-      id: '6',
-      brand: 'US.POLO',
-      image:
-        'https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/5/q/6/-original-imags487ftf3g2s7.jpeg?q=70&crop=false',
-      currentPrice: '500',
-      standardPrice: '150',
-      discount: '50',
-      name: 'Jeans',
-    },
-  ];
-
+  products: any[] = [];
   filterProduct: any[] = [];
+  productService = inject(ProductService);
   ngOnInit() {
+    this.products = this.productService.products;
     this.filterProduct = this.products;
   }
   onViewProduct(event: any) {
